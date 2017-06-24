1e77c82d06a8d2aa4d0216f801f30ea34b88ec74
ugwum <nnadiug@rocketmail.com>
2017-06-24 18:39:16 +0100

    changed typo on frontpage and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'iVoteiCheck') }}</title>
+    <title>iVoteiCheck - Create Profile</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -8,7 +8,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>iVoteiCheck</title>
+<title>iVoteiCheck - Home</title>
     <!-- Styles -->
     
     <!-- Scripts -->
