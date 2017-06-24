88b1eb89f352ded0b8a227c043d40f30eecfece4
ugwum <nnadiug@rocketmail.com>
2017-06-24 22:14:12 +0100

    removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        
                     </a>
                 </div>
 
