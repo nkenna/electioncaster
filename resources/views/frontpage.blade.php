f835eb53024925863c946eb08fee795025c03dd9
ugwum <nnadiug@rocketmail.com>
2017-06-27 00:44:40 +0100

    new

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -578,7 +578,7 @@ a img{
 <div class="android-header mdl-layout__header mdl-layout__header--waterfall">
         <div class="mdl-layout__header-row">
           <span class="android-title mdl-layout-title">
-           <img class="android-logo-image" src="/img/logo.png"> 
+           <img class="android-logo-image" src="../img/logo.png"> 
            
           </span>
           <!-- Add spacer, to align navigation to the right in desktop -->
@@ -591,7 +591,7 @@ a img{
               <input class="mdl-textfield__input" type="text" id="search-field">
             </div>
           </div>
-          
+
           <!-- Navigation -->
           <div class="android-navigation-container">
             <nav class="android-navigation mdl-navigation">
@@ -614,7 +614,7 @@ a img{
             </nav>
           </div>
           <span class="android-mobile-title mdl-layout-title">
-            <img class="android-logo-image" src="/img/logo.png"> 
+            <img class="android-logo-image" src="../img/logo.png"> 
           </span>
           <button class="android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="more-button">
             <i class="material-icons">more_vert</i>
@@ -631,7 +631,7 @@ a img{
 
        <div class="android-drawer mdl-layout__drawer">
         <span class="mdl-layout-title">
-        <img class="android-logo-image" src="/img/logo.png"> 
+        <img class="android-logo-image" src="../img/logo.png"> 
         </span>
         <nav class="mdl-navigation">
           <a class="mdl-navigation__link" href="">Home</a>
