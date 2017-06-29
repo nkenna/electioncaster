2081f89df5d00aa336b6f852524dd9ab0344a9da
ugwum <nnadiug@rocketmail.com>
2017-06-29 02:54:06 +0100

    new

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::get('user/govdataview', function () {
 })->middleware('auth');
 
 Route::get('user/updategov', function () {
-    return view('updategov');
+    return view('user/updategov');
 })->middleware('auth');
 
 //Route::get('user/dataview', function () {
@@ -70,7 +70,7 @@ Route::get('user/updategov', function () {
 
 Route::match(['get','post'],'/update', 'updateController@update')->name('update');
 
-Route::match(['get','post'],'/updateGov', 'updateGovController@updateGov')->name('updategov');
+Route::match(['get','post'],'/updateGov', 'updateGovController@updateGov')->name('success');
 
 Auth::routes();
 
